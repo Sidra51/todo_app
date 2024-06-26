a3b1a53709d2c747c8ae6428bf89fe4c5e160731
sidra <khansidra5454@gmail.com>
2024-06-27 00:49:42 +0530

    implentation of add

--- functions.py
+++ functions.py
@@ -24,18 +24,17 @@ if __name__=="__main__":
     print("helllo")
     print(get_todos())'''
 
-FILEPATH="todos1.txt"
+
 ''' so here by assigning file with diff name can help in timesaving if we need to chnage the name just we need to chmge line 27
 insted of going in gettodos and write todos function'''
-
 FILEPATH="todos1.txt"
-def get_todos(FILEPATH):
+def get_todos(filepath=FILEPATH):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, FILEPATH):
+def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
--- gui.py
+++ gui.py
@@ -1,14 +1,25 @@
 import functions
-import FreeSimpleGUI as ss
-label=ss.Text("Type in a to-do")
-input_box=ss.InputText(tooltip="enter a todo")
-add_button=ss.Button("add")
-show_button=ss.Button("show")
-edit_button=ss.Button("edit")
-complete_button=ss.Button("complete")
+import FreeSimpleGUI as sg
+label=sg.Text("Type in a to-do")
+input_box=sg.InputText(tooltip="enter a todo",key="todo")
+add_button=sg.Button("add")
 #window=ss.Window('My To_do_app', layout=[[label,input_box]])
 '''label amd input boc in one list so both will be in same row'''
-window=ss.Window('My To_do_app', layout=[[label],[input_box],[add_button],[show_button],[edit_button],[complete_button]])
+
+window=sg.Window('My To_do_app', layout=[[label],[input_box],[add_button]],font=('Helvetica',30))
 '''now input and label both have diff list so there will be diffrent row'''
-window.read()
+
+while True:
+    event,values=window.read()
+    print(event)
+    print(values)
+    match event:
+         case "add":
+             todos=functions.get_todos()
+             new_todo=values['todo'] + "\n"
+             todos.append(new_todo)
+             functions.write_todos(todos)
+         case sg.WIN_CLOSED:
+             break
+
 window.close()
